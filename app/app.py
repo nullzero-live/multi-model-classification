721dad7172a35b3663e7fd593946aba0b0162587
Nullzero <p4rlx-news@pm.me>
2023-07-02 09:30:50 +0930

    OpenAI WandB Cloud Run

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
 import os
 from data_process import gc_auth, process_data
 import streamlit as st
+import openai
+from langchain.llms import OpenAI
+
+
+ = OpenAI(model_name="text-davinci-003", openai_api_key=)
 
 def main():
     # App title
     st.title("Running multiple models in a Streamlit app")
     st.header("Logistic Regression & Random Forest Classifiers")
+    
+    api_key = st.text_input("Enter your OpenAPI Key Here...")
+    if api_key:
+        openai.api_key = api_key
+    else:
+        print("Error with API key")
+    
+    llm = OpenAI(api_key)
     
     # Query response
     textarea = st.text("Response Text",)
@@ -22,4 +35,10 @@ def main():
                     st.header(f"{classifier:}")
                     st.markdown(f"Accuracy: {accuracy:}")
                     st.markdown(f"AORC: {aorc:}")
+    summaryarea=st.text("Summary Text",)
+    summaryarea.write("This is a summary of the results...")
+    summaryarea.write(llm(f"summarize: {results}"))
+    
+    
+    
                 
