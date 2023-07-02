4398c2c983cf89165bafd6962b5c5e17021ecb85
Nullzero <p4rlx-news@pm.me>
2023-07-02 09:44:09 +0930

    OpenAI WandB Cloud Run

--- app/app.py
+++ app/app.py
@@ -37,7 +37,7 @@ def main():
                     st.markdown(f"AORC: {aorc:}")
     summaryarea=st.text("Summary Text",)
     summaryarea.write("This is a summary of the results...")
-    summaryarea.write(llm(f"summarize: "{results}))
+    summaryarea.write(llm(f"summarize: {results}"))
     
     
     
