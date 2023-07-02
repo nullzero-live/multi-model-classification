8daee09d8fad225652f97bf08eabeafceb2ee1eb
Nullzero <p4rlx-news@pm.me>
2023-07-02 12:56:18 +0930

    Cloud Run and WandB

--- app/app.py
+++ app/app.py
@@ -3,8 +3,10 @@ from data_process import gc_auth, process_data
 import streamlit as st
 import openai
 from langchain.llms import OpenAI
+from sidebar import sidebar
 
 def main():
+    sidebar()
     # App title
     st.title("Running multiple models in a Streamlit app")
     st.header("Logistic Regression & Random Forest Classifiers")
@@ -27,11 +29,11 @@ def main():
                     st.markdown(f"Accuracy: {accuracy:}")
                     st.markdown(f"AORC: {aorc:}")
     
-    st.title("Summary of Results Incoming... Please Wait.")
-    summaryarea=st.text("Summary Text",)
-    summaryarea.write("This is a summary of the results...")
-    summaryarea.write(llm(f"summarize: {results} as percentages and explain the meaning of the results as a separate paragraph. Be concise. In a new paragraph explain Accuracy and AORC in detail"))
-    
+                st.title("Summary of Results Incoming... Please Wait.")
+                summaryarea=st.text("Summary Text",)
+                summaryarea.write("This is a summary of the results...")
+                summaryarea.write(llm(f"summarize: {results} as percentages and explain the meaning of the results as a separate paragraph. Be concise. In a new paragraph explain Accuracy and AORC in detail"))
+                
     
     
                 
